994abf49fd07afd33de87b55f1daeaca1f326960
JakubKwiatek132 <126687451+JakubKwiatek132@users.noreply.github.com>
2023-03-29 12:39:31 +0000

    obramowka

--- Main.java
+++ Main.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-        Scanner o = new Scanner(System.in);
-        System.out.println("Podaj wysokosc ");
-        int height = o.nextInt();
+        
   
   
-        for (int i = 0; i < height; i++)
+        for (int i = 0; i < 5; i++)
         {
-            for(int j=0; j < height; j++)
-              System.out.print(" *");
-            System.out.println(" ");
+            for(int j=0; j < 5; j++){
+                if(i > 1 && i<4 && j>1 && j<4){
+                  System.out.print(" ");
+                }
+                else{
+                  System.out.print(" *");
+                }
+                  
+                }
+          System.out.println(" ");
+                }
+    
+              
         }
+            
+                  
+          
+            
+           
+            
+            
+        
     }
-}
